51177d9f9dd008c105f5ecc07e3847e99cd864ca
Seif Eddine Mouihbi <mouihbiseif@gmail.com>
2023-03-24 00:15:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const weatherAlert = document.getElementsByTagName("li");
+const cookieBtn = document.querySelector(".cookie");
+let switchBtn = document.querySelector(".switchtemp");
+
+function weatherplace() {
+  alert("Loading weather report ...");
+}
+
+function cookiebutton() {
+  cookieBtn.remove();
+}
+
+/* function Papayas() {
+  switchBtn.innerText = "ff";
+} */
+
+/* switch (switchBtn) {
+  case "&#8451":
+    "ff";
+    break;
+} */
+/* function cToF(celsius) 
+{
+  var cTemp = celsius;
+  var cToFahr = cTemp * 9 / 5 + 32;
+  var message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
+    console.log(message);
+} */
